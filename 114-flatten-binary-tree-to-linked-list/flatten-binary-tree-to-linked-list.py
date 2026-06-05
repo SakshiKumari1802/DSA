bd822418168a85bf34a472f2e6ddab53f7e85a23
Sakshi Kumari <150380932+SakshiKumari1802@users.noreply.github.com>
2026-06-06 00:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (76.87%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,16 @@
+class Solution:
+    def flatten(self, root):
+        curr = root
+
+        while curr:
+            if curr.left:
+                pre = curr.left
+
+                while pre.right:
+                    pre = pre.right
+
+                pre.right = curr.right
+                curr.right = curr.left
+                curr.left = None
+
+            curr = curr.right
