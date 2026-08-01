64feb83702620950cf341830fdc710a7cf77ec0f
Sakshi Kumari <150380932+SakshiKumari1802@users.noreply.github.com>
2026-08-01 16:13:13 +0530

    Time: 8 ms (11.06%) | Memory: 12.6 MB (1.10%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        INT_MIN = -2**31
+        INT_MAX = 2**31-1
+        def helper(s, i, num, sign):
+            if i>=len(s) or not s[i].isdigit():
+                return sign*num
+            num = num*10 +int(s[i])
+            if sign*num <= INT_MIN: return INT_MIN
+            if sign*num >= INT_MAX: return INT_MAX
+            return helper(s,i+1,num,sign)
+        def myAtoi(s):
+            i = 0
+            while i < len(s) and s[i] == ' ':
+                i+=1
+            sign = 1
+            if i <len(s) and (s[i] == '+' or s[i] == '-'):
+                sign = -1 if s[i] == '-' else 1
+                i += 1
+            return helper(s,i,0,sign)
+        return myAtoi(s)
+
+         
+        
