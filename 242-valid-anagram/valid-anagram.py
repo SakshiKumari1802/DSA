86cdefdb19c968a327525a9d0865076e0f33fbe2
Sakshi Kumari <150380932+SakshiKumari1802@users.noreply.github.com>
2026-08-01 12:03:25 +0530

    Time: 15 ms (86.47%) | Memory: 12.6 MB (67.51%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        freq = [0]*26
+        for char in s:
+            freq[ord(char) - ord('a')] += 1
+        for char in t:
+            freq[ord(char) - ord('a')] -= 1
+        for count in freq:
+            if count != 0:
+                return False
+        return True
+    
